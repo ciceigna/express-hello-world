5e936a76926d992d199eef3d3856a7b4c400f71d
ciceigna <ciceigna@gmail.com>
2021-05-31 16:21:47 -0300

    add index.html

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
-const port = process.ENV.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('hello world');
